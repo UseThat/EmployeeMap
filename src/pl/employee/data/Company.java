5f485af1af212f98d21cb5034b2371ff48180478
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-08-14 12:09:07 +0200

    add method addEmployee, removeEmployee to Company class

--- src/pl/employee/data/Company.java
+++ src/pl/employee/data/Company.java
@@ -8,5 +8,21 @@ import java.util.Map;
 public class Company {
     private Map<String, Employee> company = new HashMap<>();
 
+    public void addEmployee(String firstName, String lastName, double salary){
+        company.put(firstName + " " + lastName, new Employee(firstName, lastName, salary));
+        System.out.println("Employee added: "+company.get(firstName + " " + lastName));
+    }
+
+    public void removeEmployee(String key){
+        company.remove(key);
+        if(company.get(key) == null){
+            System.out.println("Employee "+ key + " removed");
+        }else{
+            System.out.println("Problem with removing employee");
+        }
+    }
+
+    public void getListOfEmployees(){
+    }
 
 }
